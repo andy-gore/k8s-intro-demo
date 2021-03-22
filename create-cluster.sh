5a286fc8272b8a117ad50488c49d965536117dfb
Andy Gore <64426235+drbungle@users.noreply.github.com>
2021-03-22 10:22:25 +0000

    Added pod spec and helper commands

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,6 +1,8 @@
 az group create --name k8sDemoResourceGroup --location westeurope
 
-az aks create --resource-group k8sDemoResourceGroup --name demoAKSCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys
+az aks create --resource-group k8sDemoResourceGroup --name myAksCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys
 
-az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
+az aks install-cli
+
+az aks get-credentials --resource-group k8sDemoResourceGroup --name myAksCluster
 
